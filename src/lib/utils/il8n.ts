824c36cdc6d361982e6afb91dd12485e6526d90c
Leynier Gutiérrez González <leynier41@gmail.com>
2024-11-16 00:38:46 -0600

    fix: improve formatting

--- src/lib/utils/il8n.ts
+++ src/lib/utils/il8n.ts
@@ -1,42 +1,42 @@
 import config from "../../../astro.config.mjs";
 
 export function getLocale(astro: {
-    currentLocale?: string;
+  currentLocale?: string;
 }): string {
-    return astro.currentLocale || config.i18n?.defaultLocale || 'en';
+  return astro.currentLocale || config.i18n?.defaultLocale || 'en';
 }
 
 export function t<T>(
-    astro: { currentLocale: string | undefined },
+  astro: { currentLocale: string | undefined },
 ): string;
 export function t<T>(
-    translations: { english: T; spanish: T },
-    locale: string | null | undefined
+  translations: { english: T; spanish: T },
+  locale: string | null | undefined
 ): T;
 export function t<T>(
-    english: T,
-    spanish: T,
-    locale: string | null | undefined
+  english: T,
+  spanish: T,
+  locale: string | null | undefined
 ): T;
 export function t<T>(
-    arg1: T | { english: T; spanish: T }  | { currentLocale?: string },
-    arg2?: T | string | null | undefined,
-    arg3?: string | null | undefined
+  arg1: T | { english: T; spanish: T } | { currentLocale?: string },
+  arg2?: T | string | null | undefined,
+  arg3?: string | null | undefined
 ): T | string {
-    let english: T;
-    let spanish: T;
-    let locale: string | null | undefined;
-    if (typeof arg1 === 'object' && arg1 !== null && 'currentLocale' in arg1) {
-        return arg1.currentLocale || config.i18n?.defaultLocale || 'en';
-    } else if (typeof arg3 === 'string') {
-        english = arg1 as T;
-        spanish = arg2 as T;
-        locale = arg3;
-    } else {
-        const translations = arg1 as { english: T; spanish: T };
-        english = translations.english;
-        spanish = translations.spanish;
-        locale = arg2 as string;
-    }
-    return locale === 'es' ? spanish : english;
+  let english: T;
+  let spanish: T;
+  let locale: string | null | undefined;
+  if (typeof arg1 === 'object' && arg1 !== null && 'currentLocale' in arg1) {
+    return arg1.currentLocale || config.i18n?.defaultLocale || 'en';
+  } else if (typeof arg3 === 'string') {
+    english = arg1 as T;
+    spanish = arg2 as T;
+    locale = arg3;
+  } else {
+    const translations = arg1 as { english: T; spanish: T };
+    english = translations.english;
+    spanish = translations.spanish;
+    locale = arg2 as string;
+  }
+  return locale === 'es' ? spanish : english;
 }
